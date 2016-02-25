64dde22fd0f174f522b4a6497a31476428ae97e3
lugDroid <lugdroid@gmail.com>
2016-02-25 20:27:44 +0000

    Common multiple solved

--- intermediate/smallest_common_multiple.js
+++ intermediate/smallest_common_multiple.js
@@ -1,54 +1,70 @@
-/*
-  Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters. The range will be an array of two numbers that will not necessarily be in numerical order.
-*/
-
 function smallestCommons(arr) {
-  var a, b;
-  // order numbers from array
+  // find smallest common multiple
+  var scm = smallestCommonMultiple(arr[0], arr[1]);
+
+  // create array of all numbers between arr[0] and arr[1]
+  var list = [];
   if (arr[0] > arr[1]) {
-    a = arr[0];
-    b = arr[1];
+    for (var i = arr[1]; i <= arr[0]; i++) {
+      list.push(i);
+    }
   } else {
-    a = arr[1];
-    b = arr[0];
-  }
-
-  // the smallest common multiple can be find if we know the greatest common divisor
-  var scm = (a * b) / greatestCommonDivisor(a, b);
-
-  // find every number between a and b
-  var numbersBetween = [];
-  for (var i = b; i <= a; i++) {
-    numbersBetween.push(i);
-  }
-  console.log(numbersBetween);
-  // check if smallest common multiple is divisible for them
-  var divisible = true;
-  for (var j = 0; j < numbersBetween.length; j++) {
-    if (!scm % numbersBetween[j] === 0) {
-      divisible = false;
+    for (var j = arr[0]; j <= arr[1]; j++) {
+      list.push(j);
     }
   }
 
-  console.log(divisible);
-  return scm;
+  // divide smallest common multiple and multiples until one of them
+  // is divisible between all numbers in array
+  var result = 0;
+  var module;
+  do {
+    module = 0;
+    result += scm;
+    // console.log(result);
+    for (var k = 0; k < list.length; k++) {
+      module += result % list[k] !== 0;
+      // console.log(module, list[k]);
+    }
+  } while (module !== 0);
+
+  return result;
 }
 
-console.log("smallestCommons(1, 5) = 5 =? " + smallestCommons([1,5]));
-console.log("smallestCommons(72, 50) = 1800 =? " + smallestCommons([72, 50]));
+
+console.log("smallestCommons([1, 5]) = 60 =? " + smallestCommons([1, 5]));
+console.log("smallestCommons([5, 1]) = 60 =? " + smallestCommons([5, 1]));
+console.log("smallestCommons([1, 13]) = 360360 =? " + smallestCommons([1, 13]));
 
 function greatestCommonDivisor(a, b) {
   /*
-    return greatest common divisor of given numbers
-    assume given numbers are ordered (a > b)
+    return the greates common divisor of a and b
+    assume the numbers are ordered; a > b
   */
+
   if ((a % b) === 0) {
-    return (b);
+    return b;
   } else {
     return greatestCommonDivisor(b, (a % b));
   }
 }
 
-// console.log("greatestCommonDivisor(60, 48) = 12 =? " + greatestCommonDivisor(60, 48));
-// console.log("greatestCommonDivisor(56, 42) = 14 =? " + greatestCommonDivisor(56, 42));
-// console.log("greatestCommonDivisor(50, 72) = 2 =? " + greatestCommonDivisor(50, 72));
+//console.log("gcd(60,48) = 12 =? " + greatestCommonDivisor(60, 48));
+//console.log("gcd(56,42) = 14 =? " + greatestCommonDivisor(56, 42));
+//console.log("gcd(2366,273) = 91 =? " + greatestCommonDivisor(2366, 273));
+
+function smallestCommonMultiple(a, b) {
+  /*
+    return the smallest common multiple of a and b
+    depends on greatestCommonDivisor()
+  */
+  if (a > b) {
+    return ((a * b) / greatestCommonDivisor(a, b));
+  } else {
+    return ((a * b) / greatestCommonDivisor(b, a));
+  }
+}
+
+// console.log("scm(1,5) = 5 =? " + smallestCommonMultiple(1, 5));
+// console.log("scm(7,3) = 21 =? " + smallestCommonMultiple(7, 3));
+// console.log("scm(50,72) = 1800 =? " + smallestCommonMultiple(50, 72));
